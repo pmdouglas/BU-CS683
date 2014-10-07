bc72aded9fec105b007e9bbdb6c9f824641c3dbc
pmdouglas <pmdouglas@gmail.com>
2014-10-07 02:04:36 -0400

    homework2
    homework 2 showing a menu item

--- GeoQuiz/src/com/bu/cs683/geoquiz/MainActivity.java
+++ GeoQuiz/src/com/bu/cs683/geoquiz/MainActivity.java
@@ -5,24 +5,57 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	private Button mTrueButton;
+	private Button mFalseButton;
+	private Button mNextButton;
+	private TextView mQuestionTextView;
+	
+	private TrueFalse[] mQuestionBank = new TrueFalse[]{
+			new TrueFalse(R.string.question_oceans,true),
+			new TrueFalse(R.string.question_mideast,false),
+			new TrueFalse(R.string.question_africa,false),
+			new TrueFalse(R.string.question_americas,true),
+			new TrueFalse(R.string.question_asia,true),
+	};
+	private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        Button mTrueButton = (Button)this.findViewById(R.id.true_button);
-        Button mFalseButton = (Button)this.findViewById(R.id.false_button);
+        mTrueButton = (Button)this.findViewById(R.id.true_button);
+        mFalseButton = (Button)this.findViewById(R.id.false_button);
+        mQuestionTextView = (TextView)this.findViewById(R.id.question_text_view);
+        mNextButton = (Button)this.findViewById(R.id.next_button);
         
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-            	Toast toast = Toast.makeText(getBaseContext(), "Correct!", 10);
-            	toast.show();
+            	checkAnswer(true);
             }
         });
+        
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				checkAnswer(false);
+			}
+		});
+        
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				mCurrentIndex = (mCurrentIndex + 1)% mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+        
+        updateQuestion();
     }
 
 
@@ -31,5 +64,20 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    private void updateQuestion(){
+    	mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getQuestion());
+    }
+    
+    private void checkAnswer(boolean userPressedTrue){
+    	boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+    	int messageResId=0;
+    	if(userPressedTrue == answerIsTrue){
+    		messageResId = R.string.correct_text;
+    	}else{
+    		messageResId = R.string.incorrect_text;
+    	}
+    	Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ RestaurantFinder/gen/bu/cs683/restaurantfinder/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package bu.cs683.restaurantfinder;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
